c8f878edaa0afc39c7bee69e5dbbf7c91b0da467
Marco Villano <91207733+M4RV1L@users.noreply.github.com>
2025-08-03 17:02:06 +0200

    Update Footer.jsx

--- src/app/compnents/Footer.jsx
+++ src/app/compnents/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className='footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
         <div className='container p-12 flex justify-between'>
-              <span>made by M4RV1L - <Link href={""} className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-600'>/src</Link></span>
+              <span>made by M4RV1L - <Link href={"https://m4rv1l.xyz/"} className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-600'>/src</Link></span>
             <p className='text-slate-600'>All rights reserved.</p>
         </div>
     </footer>
